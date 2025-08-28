776a64e3cbd029201182f5ec4bf2975c0ec1dc0d
Tharuka Sooriyaarachchi <contact@tharuka.dev>
2025-08-28 19:59:30 +0530

    fixed sales summery

--- src/app/modules/dashboard/components/dashboard-view/dashboard-view.component.ts
+++ src/app/modules/dashboard/components/dashboard-view/dashboard-view.component.ts
@@ -60,6 +60,7 @@ export class DashboardViewComponent implements AfterViewInit, OnDestroy {
 
     statisticsService = inject(StatisticsService)
 
+    openSummary = false;
 
     // Dashboard metrics
     totalRevenue = 0;
